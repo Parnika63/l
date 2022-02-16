fe7c66cade07258f173e519c943fe668a137efb0
Parnika63 <80901046+Parnika63@users.noreply.github.com>
2022-02-16 19:23:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -61,11 +61,6 @@ function gotPoses(results)
   }
 }
 
-function startGame()
-{
-  game_status = "start";
-  document.getElementById("status").innerHTML = "Game Is Loading";
-}
 
 function draw(){
 
